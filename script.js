27e041cb5a8ed64d171ec993f96f7ad37c271540
SrdjanLakic <srdjan949@gmail.com>
2022-03-28 21:09:48 -0400

    Disabled check button after lost game

--- script.js
+++ script.js
@@ -9,6 +9,8 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+const number = document.querySelector('.number');
+let button = document.querySelector('.check');
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
@@ -24,9 +26,9 @@ document.querySelector('.check').addEventListener('click', function () {
     displayMessage('⛔️ No Number!');
   } else if (guess === secretNumber) {
     displayMessage('Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
+    number.textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+    number.style.width = '30rem';
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
@@ -38,16 +40,19 @@ document.querySelector('.check').addEventListener('click', function () {
       displayScore(score);
     } else {
       displayMessage('💥You lost the game!');
+
+      button.disabled = true;
     }
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
+  button.disabled = false;
   displayMessage('Start guessing...');
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').textContent = '?';
+  number.style.width = '15rem';
+  number.textContent = '?';
   document.querySelector('.guess').value = '';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   displayScore(score);
